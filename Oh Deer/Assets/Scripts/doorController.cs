f63c3a523f2fb587c0a8e087d545e2310b2e63f6
nathan <nathannash11@gmail.com>
2019-01-26 16:25:47 +0000

    door stuff

---
+++ Oh Deer/Assets/Scripts/doorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    public doorController nextRoom;
+    public GameObject currentRoom;
+    public Transform playerSpawn;//spawn for current room
+
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //open new room
+        nextRoom.transform.parent.gameObject.SetActive(true);
+        //reposition player
+        player.transform.position = nextRoom.playerSpawn.position;
+        //hide current room
+        currentRoom.gameObject.SetActive(false);
+    }
+
+}
