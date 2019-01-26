4e15497f27209afdcaca14b8e151629cc66fe3bf
nathan <nathannash11@gmail.com>
2019-01-26 17:38:08 +0000

    doors

---
+++ Oh Deer/Assets/miniGameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class miniGameController : MonoBehaviour
+{
+    public Scene miniGame;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.SetActiveScene(miniGame);
+    }
+}
